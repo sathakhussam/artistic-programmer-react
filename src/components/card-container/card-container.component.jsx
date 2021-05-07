c57010c6578674a3e4c266a696eca6160c402a42
Sathak Uzham <sathakhussam@gmail.com>
2021-05-07 14:06:00 +0530

    Added save functionality

--- src/components/card-container/card-container.component.jsx
+++ src/components/card-container/card-container.component.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
 import './card-container.styles.scss'
 import PostCard from '../post-card/post-card.component'
+import { v4 as uuidv4 } from 'uuid';
+
 
 class CardComponent extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            
+            posts: [{id: uuidv4()}, {id: uuidv4()}, {id: uuidv4()}]
         }
     }
+
 
     render() {
         return (
             <div className="card-container">
-                <PostCard/>
-                <PostCard/>
-                <PostCard/>
+                {
+                    this.state.posts.map((post, idx)=> (
+                        <PostCard key={post.id} {...post}/>
+                    ))
+                }
             </div>
         )
     }
--- src/components/post-card/post-card.component.jsx
+++ src/components/post-card/post-card.component.jsx
@@ -1,25 +1,48 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './post-card.styles.scss'
 
-function PostCard() {
+function PostCard(props) {
     const [saved, editSaved] = React.useState(false)
+    const [id,] = React.useState(props.id)
+
+    React.useEffect(() => {
+        return () => {
+            var savedPostsId = JSON.parse(localStorage.getItem("savedPostsId"))
+            editSaved(savedPostsId.includes(id))
+        }
+    }, [saved, id])
+    
+    React.useEffect(() => {
+        var savedPostsId = JSON.parse(localStorage.getItem("savedPostsId"))
+        if (saved){            
+            localStorage.setItem("savedPostsId",savedPostsId ? JSON.stringify([...savedPostsId, id]) : JSON.stringify([id]))
+        } else if (!saved ) {
+            if (savedPostsId) savedPostsId = savedPostsId.filter((val) => val !== id)
+            localStorage.setItem("savedPostsId",JSON.stringify(savedPostsId))
+
+        }
+    }, [saved, id])
+
     return (
         <div className="post-card">
-            <div className="content">
-                <div className="save-btn">
-                <i onClick={() => editSaved(!saved)} class={`fa${saved ? 's' : 'r' } fa-bookmark`}></i>
-                </div>
-                <div className="text-content">
-                    <h3>Introduction to Bootstrap</h3>
-                    <div className="text-post--info">
-                        <p>By Sathak Uzham</p>
-                        <p>17 July 2021</p>
-                    </div>
-                    <a className="btn btn-primary txt-bold">READ MORE</a>
-                </div>
-            </div>
+        <div className="content">
+
+        <div className="save-btn">
+        <i onClick={() => editSaved(!saved)} className={`fa${saved ? 's' :'r' } fa-bookmark`}></i>
         </div>
+        <div className="text-content">
+            <h3>Introduction to Bootstrap</h3>
+            <div className="text-post--info">
+                <p>By Sathak Uzham</p>
+                <p>17 July 2021</p>
+            </div>
+            <a className="btn btn-primary txt-bold">READ MORE</a>
+        </div>
+        </div>
+
+    </div>
     )
 }
 
